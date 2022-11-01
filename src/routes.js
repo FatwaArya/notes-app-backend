523c2e75016ef3b1c7ad87f66e3e95dac82c3290
Fatwa <fatwaaryasatyaakbar@gmail.com>
2022-11-01 17:55:43 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import {
+  addNoteHandler,
+  editNoteByIdHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  deleteNoteByIdHandler,
+} from "./handler.js";
+
+const routes = [
+  {
+    path: "/notes",
+    method: "POST",
+    handler: addNoteHandler,
+  },
+  {
+    path: "/notes",
+    method: "GET",
+    handler: getAllNotesHandler,
+  },
+  {
+    path: "/notes/{id}",
+    method: "GET",
+    handler: getNoteByIdHandler,
+  },
+  {
+    path: "/notes/{id}",
+    method: "PUT",
+    handler: editNoteByIdHandler,
+  },
+  {
+    path: "/notes/{id}",
+    method: "DELETE",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+export default routes;
